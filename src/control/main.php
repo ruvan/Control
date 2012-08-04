3adbbbfc903d79b38938114e43dd8266b2d7f22e
Tyrone <Tyrone@10.0.2.15>
2012-08-04 19:00:16 +0800

    Made table for config changes

--- src/control/main.php
+++ src/control/main.php
@@ -11,12 +11,15 @@ if(isset($_POST['config'])) {
 // Read and display config file
 if($_SESSION['userlevel']==1) {
     $config=fopen("config.properties","r") or exit("Unable to open config file!");
-    $lastModTime = fgets($config);
-    echo "Last modification time " . $lastModTime;
+    $lastModTime = explode(" ",fgets($config));
+    echo "<table border='1'>";
+    //echo "Last modification time " . $lastModTime;
+    echo "<form action='main.php' method='post'> <br> \n";
     while (!feof($config)) {
         $line = explode("=",fgets($config));
-        echo $line[0] . " = " . $line[1] . "<br>";
+        echo "<tr> <td> $line[0] </td> <td> <input type='text' name='$line[0]' value='$line[1]' /> </td> </tr> \n";
     }
+    echo "<tr> <td> Last mod: $lastModTime[1] $lastModTime[2] $lastModTime[3]</td> <td> <input type='submit' value='Submit' /> </td>  </tr> \n </table>";
     fclose($config);
 }
 
